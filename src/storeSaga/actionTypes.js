964d163132f6cd897f06676fd78c983fe2bcc790
lily-elephant <xrzslixiangqun@163.com>
2020-06-26 18:26:19 +0800

    整理

--- src/storeSaga/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const CHANGE_INPUT_VALUE = 'change_input_value';
-export const SUBMIT_INPUT_VALUE = 'submit_input_value';
-export const DELETE_ITEM = 'delete_item';
-export const GET_LIST_DATA = 'get_list';
-export const GET_INIT_LIST = 'get_init_list';
--- src/third/TodoListUI.js
+++ src/third/TodoListUI.js
@@ -1,6 +1,11 @@
 import React, { Fragment} from 'react'
 
-// UI组件
+/*
+* UI组件不涉及业务逻辑，无生命周期函数，只负责单纯的渲染，所有数据都通过 props 传入。
+* UI组件分为两类， 无状态组件和有状态组件
+* 无状态组件使用纯函数
+* 有状态组件使用react的PureComponent，可能会在有状态组件中定义一些方法，以避免render中绑定this带来的性能消耗
+* */
 // class TodoListUI extends Component {
 //   render() {
 //     return (
